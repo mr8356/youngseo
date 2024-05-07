c228e545a37fa33632424d8bda7f39f0ac7d3447
mr8356 <mr8356@naver.com>
2024-05-07 18:25:59 +0900

    new

--- test.c
+++ test.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 int main(){
     printf("hello world!!!!!!!!");
+    printf("hello world!!!!!!!!");
+    v
+    printf("hello world!!!!!!!!");
+    printf("hello world!!!!!!!!");
+    printf("hello world!!!!!!!!");
+    printf("hello world!!!!!!!!");
+    printf("hello world!!!!!!!!");
+    printf("hello world!!!!!!!!");
+    
 }
